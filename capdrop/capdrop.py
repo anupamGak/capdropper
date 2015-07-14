6c38ddec3139ac7ae10bd457a4d618ec1765a743
anupamGak <anupamgak07@gmail.com>
2015-07-14 14:35:27 +0530

    Perfected CSS for dropcap

--- capdrop/capdrop.py
+++ capdrop/capdrop.py
@@ -1,16 +1,16 @@
 import zipfile
 from lxml import html, etree
 import re
-from os.path import dirname, relpath
+from os.path import *
 
 
 class CapDrop:
 	def __init__(self, filename, fontfile):
 		self.inbook = zipfile.ZipFile(filename, 'r')
-		self.outbook = zipfile.ZipFile('(NEW)_' + filename, 'w')
+		newbase = "(NEW)_" + basename(filename)
+		self.outbook = zipfile.ZipFile(join(dirname(filename), newbase), 'w')
 
 		self.filelist = self.inbook.namelist()
-		self.dir = dirname([i for i in self.filelist if 'htm' in i][0])
 		self.fontfile = fontfile
 
 		self.add_styledata()
@@ -28,7 +28,7 @@ class CapDrop:
 				break
 			else:
 				for child in para.xpath("descendant::*"):
-					if child.text and re.search(r"\w", child.text) and len(child.text) > 15:
+					if child.text and re.search(r"\w", child.text) and len(child.text) > 50:
 						intro = child
 						chosen = True
 						break
@@ -36,8 +36,9 @@ class CapDrop:
 					break
 
 		if chosen:
-			cont = intro.text[0]
-			tail = intro.text[1:]
+			pos = re.search(r"\S",intro.text).start()
+			cont = intro.text[pos:pos+1]
+			tail = intro.text[pos+1:]
 			intro.text = ""
 
 			dc_span = etree.Element("span")
@@ -53,6 +54,8 @@ class CapDrop:
 		self.outbook.write(self.fontfile, 'dropcap-adder/fonts/dropcap-font.ttf')
 
 		content_file = [i for i in self.filelist if 'content.opf' in i][0]
+		with self.inbook.open(content_file, 'r') as cxml:
+			line = cxml.readline()
 		content = html.fromstring(self.inbook.read(content_file))
 		manif = content.xpath("//manifest")[0]
 
@@ -63,7 +66,6 @@ class CapDrop:
 			"href" : relpath("dropcap-adder/fonts/dropcap-font.ttf", content_file)[3:],
 			"media-type" : "application/x-font-truetype"
 		})
-		print "Font", relpath("dropcap-adder/fonts/dropcap-font.ttf", content_file)[3:]
 		itemcss = etree.SubElement(manif, "item")
 		itemcss.attrib.update({
 			"id" : "dropcap-css",
@@ -71,7 +73,7 @@ class CapDrop:
 			"media-type" : "text/css"
 		})
 
-		self.outbook.writestr(content_file, html.tostring(content))
+		self.outbook.writestr(content_file, line + etree.tostring(content))
 
 	def edit_htmls(self):
 		htmfiles = [i for i in self.filelist if 'htm' in i]
@@ -88,7 +90,7 @@ class CapDrop:
 				"type" : "text/css"
 			})
 
-			self.outbook.writestr(htmfile, html.tostring(tree))
+			self.outbook.writestr(htmfile, etree.tostring(tree))
 
 	def complete(self):
 		remfiles = [i for i in self.filelist if i not in self.outbook.namelist()]
@@ -96,5 +98,9 @@ class CapDrop:
 		for file in remfiles:
 			self.outbook.writestr(file, self.inbook.read(file))
 
+		print "File writing completed"
 
-drop = CapDrop('Lolita - Vladimir Nabokov.epub', 'resources/dropcap.ttf')
+
+drop = CapDrop("samples/Do Androids Dream of Electric Sheep - Philip K. Dick.epub", 'resources/dropcap.ttf')
+drop2 = CapDrop("samples/Lolita - Vladimir Nabokov.epub", 'resources/dropcap.ttf')
+drop3 = CapDrop("samples/Philip Reeve - [Mortal Engines 05 - Fever Crumb 01] - Fever Crumb (epub).epub", 'resources/dropcap.ttf')
